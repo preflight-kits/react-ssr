0f3189479f75eddaac75d2293cca6aa39513dc8c
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2023-05-06 21:02:59 +0330

    feat: add sass and css loader

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/client/index.tsx',
   output: {
-    filename: 'client.js',
+    filename: 'js/client.min.js',
     path: path.resolve(__dirname, 'dist/public'),
   },
   module: {
@@ -14,11 +15,47 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                auto: true,
+                localIdentName: "[hash:base64:5]",
+              },
+            }
+          }
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                auto: true,
+                localIdentName: "[hash:base64:5]",
+              },
+            }
+          },
+          'sass-loader'
+        ],
+      },
     ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
     plugins: [new TsconfigPathsPlugin()]
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/client.min.css',
+    }),
+  ]
 };
 
--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/server/index.tsx',
@@ -16,6 +17,37 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                auto: true,
+                localIdentName: "[hash:base64:5]",
+              },
+            }
+          }
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                auto: true,
+                localIdentName: "[hash:base64:5]",
+              },
+            }
+          },
+          'sass-loader'
+        ],
+      },
     ],
   },
   resolve: {
@@ -24,4 +56,9 @@ module.exports = {
   },
   externalsPresets: { node: true },
   externals: [nodeExternals()],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'public/css/client.min.css',
+    }),
+  ]
 };
